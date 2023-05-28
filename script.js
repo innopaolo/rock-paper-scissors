e61e55da9d329e0e22e7c0f93ea535840a7fb354
Inno Paolo <102833497+innopaolo@users.noreply.github.com>
2023-05-28 17:38:31 +0600

    Create UI for the game and refactor Javascript

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function cycleImages(computerSelection) {
+    let imageArray = ["rock.png", "paper.png", "scissors.png"];
+    let imageIndex = 0;
+    let imageElement = document.querySelector("#cycle");
+
+    const cycle = setInterval (function() {
+        imageIndex = (imageIndex + 1) % imageArray.length;
+        imageElement.src = imageArray[imageIndex];
+    }, 50);
+
+    setTimeout(function() {
+    clearInterval(cycle);
+    imageElement.src = `${computerSelection}.png`;
+    }, 3000);
+}
+
 function getComputerChoice() {
     let choices = ["rock", "paper", "scissors"];
     let x = Math.random() * choices.length;
@@ -6,76 +22,91 @@ function getComputerChoice() {
 
 let playerScore = 0;
 let computerScore = 0;
+let counter = 0;
+let isGameOver = false;
+let rounds = document.querySelector(".rounds");
 
-function playRound(playerSelection, computerSelection) {
-    
-    let player = playerSelection.toLowerCase();
-    
+function playRound(player, computerSelection) {
+
+    let para = document.createElement("p");
 
     if (player === "rock") {
         if (computerSelection === "rock") {
-            console.log("It's a tie! You chose the same weapon.");
+            para.textContent = "It's a tie! Same weapons.";
         } else if (computerSelection === "paper") {
-            console.log("You lose! Paper beats rock.");
+            para.textContent = "You lose! Paper beats rock.";
             computerScore += 1;
         } else if (computerSelection === "scissors") {
-            console.log("You win! Rock beats scissors.");
+            para.textContent = "You win! Rock beats scissors.";
             playerScore += 1;
         }
     } else if (player === "paper") {
         if (computerSelection === "rock") {
-            console.log("You win! Paper beats rock.");
+            para.textContent = "You win! Paper beats rock.";
             playerScore += 1;
         } else if (computerSelection === "paper") {
-            console.log("It's a tie! You chose the same weapon.");
+            para.textContent = "It's a tie! Same weapons.";
         } else if (computerSelection === "scissors") {
-            console.log("You lose! Scissors beat paper.");
+            para.textContent = "You lose! Scissors beat paper.";
             computerScore += 1;
         }
     } else if (player === "scissors") {
         if (computerSelection === "rock") {
-            console.log("You lose! Rock beats scissors.");
+            para.textContent = "You lose! Rock beats scissors.";
             computerScore += 1;
         } else if (computerSelection === "paper") {
-            console.log("You win! Scissors beat paper.");
+            para.textContent = "You win! Scissors beat paper.";
             playerScore += 1;
         } else if (computerSelection === "scissors") {
-            console.log("It's a tie! You chose the same weapon.");
+            para.textContent = "It's a tie! Same weapons.";
         }
-    } else {
-        console.log("Either you misspelled it or not a valid weapon! Try again.");
-    }               
-} 
-
-let counter = 5;
-
-
-function game() {
-    for (let i = 0; i < counter; i++) {  
-        let playerSelection = String(prompt("Choose a weapon:"));
-
-        if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors"){
-            i = i - 1; // Round is not counted if player doesn't select any of the choices
-        }
-
-        let computerSelection = getComputerChoice();
-
-        console.log(playRound(playerSelection, computerSelection));
     }
 
-    console.log(`Player: ${playerScore}`);
-    console.log(`Computer: ${computerScore}`);
+    setTimeout (function() {
+        para.style.fontWeight = "bold";
+        rounds.appendChild(para);
 
-    if (playerScore > computerScore) {
-        console.log("Congratulations! You won the war.");
-    } else if (playerScore < computerScore) {
-        console.log("I'm sorry, you suck!!!");
-    } else {
-        console.log("It's a mexican standoff.");
-    }
+        if (counter === 2 && !isGameOver) {
+            isGameOver = true;
 
+            let result = document.createElement("p");
+            result.style.backgroundColor = "#ff5853";
+            result.style.padding = "10px";
+            result.style.borderRadius = "8px";
+            result.style.fontWeight = "bolder";
+
+            if (playerScore > computerScore) {
+                result.textContent = "Congratulations! You won the war.";
+            } else if (playerScore < computerScore) {
+                result.textContent = "I'm sorry, you suck!!!";
+            } else {
+                result.textContent = "It's a mexican standoff.";
+            }
+
+            rounds.appendChild(result);
+        }
+
+        counter++; // After the if statement to ensure ifBlock prints after third round
+    }, 3000);
 }
 
-game();
+let isCooldown = false;
 
+const weapons = Array.from(document.querySelectorAll(".weapons div"));
+weapons.forEach((weapon) => weapon.addEventListener("click", function() {
+    if (counter < 3) {
+        if (isCooldown) return;
+        isCooldown = true;
 
+        let playerSelection = weapon.classList.value;
+        let computerSelection = getComputerChoice();
+
+        cycleImages(computerSelection);
+
+        setTimeout (() => {
+            isCooldown = false;
+        }, 3000);
+
+        playRound(playerSelection, computerSelection);
+    };
+}));
